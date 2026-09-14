59830b1a94a34086d5bc250235cdb45102f7e51f
viieiramd <mdudamv7@gmail.com>
2026-09-14 15:49:41 -0300

    Implementa listagem de produtos vendidos

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -57,7 +57,6 @@ public class ProdutosDAO {
             conn = new conectaDAO().connectDB();
 
             prep = conn.prepareStatement(sql);
-
             resultset = prep.executeQuery();
 
             while (resultset.next()) {
@@ -100,5 +99,39 @@ public class ProdutosDAO {
                 "Erro ao vender produto: " + erro.getMessage()
             );
         }
+    }
+
+    public ArrayList<ProdutosDTO> listarProdutosVendidos() {
+
+        ArrayList<ProdutosDTO> vendidos = new ArrayList<>();
+
+        String sql = "SELECT * FROM produtos WHERE status = 'Vendido'";
+
+        try {
+            conn = new conectaDAO().connectDB();
+
+            prep = conn.prepareStatement(sql);
+            resultset = prep.executeQuery();
+
+            while (resultset.next()) {
+
+                ProdutosDTO produto = new ProdutosDTO();
+
+                produto.setId(resultset.getInt("id"));
+                produto.setNome(resultset.getString("nome"));
+                produto.setValor(resultset.getInt("valor"));
+                produto.setStatus(resultset.getString("status"));
+
+                vendidos.add(produto);
+            }
+
+        } catch (Exception erro) {
+            JOptionPane.showMessageDialog(
+                null,
+                "Erro ao listar produtos vendidos: " + erro.getMessage()
+            );
+        }
+
+        return vendidos;
     }
 }
