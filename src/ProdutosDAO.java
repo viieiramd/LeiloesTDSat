0812be226da2d1c10d96f9db9eb50ddc6887bb5d
viieiramd <mdudamv7@gmail.com>
2026-09-14 15:41:55 -0300

    Implementa operacao venderProduto

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -81,4 +81,24 @@ public class ProdutosDAO {
 
         return listagem;
     }
+
+    public void venderProduto(int id) {
+
+        String sql = "UPDATE produtos SET status = 'Vendido' WHERE id = ?";
+
+        try {
+            conn = new conectaDAO().connectDB();
+
+            prep = conn.prepareStatement(sql);
+            prep.setInt(1, id);
+
+            prep.executeUpdate();
+
+        } catch (Exception erro) {
+            JOptionPane.showMessageDialog(
+                null,
+                "Erro ao vender produto: " + erro.getMessage()
+            );
+        }
+    }
 }
