7d0e2743d9f204e500ec0bb1494920900e76a379
viieiramd <mdudamv7@gmail.com>
2026-09-14 14:52:32 -0300

    Implementa listagem de produtos cadastrados

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -35,7 +35,11 @@ public class ProdutosDAO {
             prep.setString(3, produto.getStatus());
 
             prep.executeUpdate();
-            JOptionPane.showMessageDialog(null, "Produto cadastrado com sucesso!");
+
+            JOptionPane.showMessageDialog(
+                null,
+                "Produto cadastrado com sucesso!"
+            );
 
         } catch (Exception erro) {
             JOptionPane.showMessageDialog(
@@ -46,6 +50,34 @@ public class ProdutosDAO {
     }
 
     public ArrayList<ProdutosDTO> listarProdutos() {
+
+        String sql = "SELECT * FROM produtos";
+
+        try {
+            conn = new conectaDAO().connectDB();
+
+            prep = conn.prepareStatement(sql);
+
+            resultset = prep.executeQuery();
+
+            while (resultset.next()) {
+
+                ProdutosDTO produto = new ProdutosDTO();
+
+                produto.setId(resultset.getInt("id"));
+                produto.setNome(resultset.getString("nome"));
+                produto.setValor(resultset.getInt("valor"));
+                produto.setStatus(resultset.getString("status"));
+
+                listagem.add(produto);
+            }
+
+        } catch (Exception erro) {
+            JOptionPane.showMessageDialog(
+                null,
+                "Erro ao listar produtos: " + erro.getMessage()
+            );
+        }
 
         return listagem;
     }
